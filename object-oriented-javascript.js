e531920097f9c335d4ca6b595dc501b2a678b6c8
WigglyDon <donfather1000@gmail.com>
2023-12-14 14:49:51 -0700

    add class method and another exercise completed

--- object-oriented-javascript.js
+++ object-oriented-javascript.js
@@ -14,3 +14,22 @@ const myPerson = new Person("Ziggy", "Zoggy");
 console.log(myPerson.fullName())
 
 console.log(`created with ${myPerson.firstName} && ${myPerson.lastName}`);
+
+
+// Create a Person class which accepts the name and age of a person.
+// The Person class should have a *describe* method which returns a string =>
+// `${name}, ${age} years old`.
+
+class PersonTwo {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    describe() {
+        return `${this.name}, ${this.age} years old`
+    }
+}
+
+ const myOtherPerson = new PersonTwo("Billy the Kid", 55);
+
+ console.log(`${myOtherPerson.describe()}`)
