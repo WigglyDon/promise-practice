a8c5c4933b05988bff69e95f2a341b734e0807c7
WigglyDon <donfather1000@gmail.com>
2023-12-14 14:23:37 -0700

    add notes for async-await

---
+++ async-await-1.js
@@ -0,0 +1,22 @@
+// async and await are meant to make async programming easy by introducing
+// "coroutines". A coroutine is a function which can pause it's execution
+// and return control to the main loop until some event occurs.
+
+// It is an alternative approach to using callbacks, which makes it
+// easier to write, understand, and maintain! :)
+
+// *await*
+// await is a special command, which stops the execution of the
+// current function until a Promise resolves, and then return
+// the promises value. It can be seen as an endless loop which
+// checks if the promise has been resolved, and returns the value
+// of the resolved promise when it does.
+
+// await only works inside of async functions (which are coroutines),
+// It's kind of tricky because they return a promise, instead of a value.
+// Therefore, every time an async function is run, it needs an await
+// in order to get the return value.
+
+// Going to also try adding *sleep()* here instead of *setTimeout()*.
+// sleep() still uses setTimeout() to work.
+
